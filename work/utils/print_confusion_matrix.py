d07413aeae512df8addef4f331aad24b07cdbb86
Elia Corò <e.coro@opinioni.net>
2025-02-05 16:32:33 +0100

    asd

--- work/utils/print_confusion_matrix.py
+++ work/utils/print_confusion_matrix.py
@@ -2,12 +2,15 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import numpy as np
 import matplotlib.pyplot as plt
 
-def print_confusion_matrix(y_test_original, y_pred):
+def print_confusion_matrix(y_test_original, y_pred, title = ""):
     # Matrice di confusione
     conf_matrix = confusion_matrix(y_test_original, y_pred)
+
+    if title == "":
+        title = "Confusion Matrix"
     
     # Visualizzazione
     disp = ConfusionMatrixDisplay(confusion_matrix=conf_matrix, display_labels=np.arange(4))
     disp.plot(cmap=plt.cm.Blues)
-    plt.title("Confusion Matrix")
+    plt.title(title)
     plt.show()
